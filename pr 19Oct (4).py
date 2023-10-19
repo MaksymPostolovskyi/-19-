bc5cae1008d2f617b855798b325f21a4cc68f986
MP <144225366+MaksymPostolovskyi@users.noreply.github.com>
2023-10-19 21:17:56 +0300

    Add files via upload

---
+++ pr 19Oct (4).py
@@ -0,0 +1,32 @@
+list = [1 , 2, 3, 4, 5, 6,]
+
+if 2 in list:
+    list.remove(3)
+
+print(list)
+
+
+
+list = [1, 2, 3, 4, 5]
+print(list)
+
+list.extend([3, 6, 7])
+print(list)
+
+
+list.insert(1, 33333)
+print(list)
+
+list.reverse()
+print(list)
+
+
+list.append(3)
+print(list)
+
+list.sort()
+print(list)
+
+list.clear()
+
+print(list)
